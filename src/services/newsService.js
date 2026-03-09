26d09f2fbb56cb08a35ff38e0e560eb3d70d351f
Satyam Prakash <satyamprakash2828@gmail.com>
2026-03-09 22:22:06 +0000

    add env variable

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
 const API = "http://localhost:8080/news"
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export async function fetchNews() {
-  const res = await axios.get("/news")
+  const res = await axios.get(BASE_URL + "/news")
   return res.data
 }
